dd56da736c98a9a15c16eef739136db6f5510662
GoeunLee <lgy0109@naver.com>
2020-07-14 18:15:46 +0900

    clock.js 완료

---
+++ clock.js
@@ -0,0 +1,18 @@
+
+const clockContainer = document.querySelector(".js-clock"),
+clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+const date = new Date();
+const minutes = date.getMinutes();
+const hours = date.getHours();
+const seconds = date.getSeconds();
+clockTitle.innerText = `${hours > 12 ? `${hours-12}` : hours}:${minutes < 10 ? `0${minutes}`: minutes}:${seconds < 10 ? `0${second}`: seconds}`;
+}
+
+function init() {
+getTime();
+setInterval(getTime,1000);
+}
+
+init();
